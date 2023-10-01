40558b26c23d5a87efb0c4ebd87b32f7d1bb2f4a
windsing <117965575+wochenlong@users.noreply.github.com>
2023-10-02 03:47:15 +0800

    汉化+注释

--- pixiv_crawler/downloader/downloader.py
+++ pixiv_crawler/downloader/downloader.py
@@ -1,4 +1,3 @@
-
 import concurrent.futures as futures
 from typing import Iterable, Set
 
@@ -9,35 +8,39 @@ from utils import printInfo
 from .download_image import downloadImage
 
 
-class Downloader():
-    """[summary]
-    download controller
+class Downloader:
+    """
+    下载控制器
     """
 
     def __init__(self, capacity):
-        self.url_group: Set[str] = set()
+        self.url_group: Set[str] = set()  # 存储要下载的图片 URL 集合
         self.capacity = capacity
 
     def add(self, urls: Iterable[str]):
+        """
+        添加要下载的图片 URL
+        """
         for url in urls:
             self.url_group.add(url)
 
     def download(self):
-        flow_size = .0
-        printInfo("===== downloader start =====")
+        """
+        执行下载操作
+        """
+        flow_size = 0.0  # 记录下载的流量大小
+        printInfo("===== 下载开始 =====")
 
-        n_thread = DOWNLOAD_CONFIG["N_THREAD"]
+        n_thread = DOWNLOAD_CONFIG["N_THREAD"]  # 并发下载线程数
         with futures.ThreadPoolExecutor(n_thread) as executor:
-            with tqdm(total=len(self.url_group), desc="downloading") as pbar:
-                for image_size in executor.map(
-                        downloadImage, self.url_group):
+            with tqdm(total=len(self.url_group), desc="正在下载") as pbar:
+                for image_size in executor.map(downloadImage, self.url_group):
                     flow_size += image_size
                     pbar.update()
-                    pbar.set_description(
-                        f"downloading / flow {flow_size:.2f}MB")
+                    pbar.set_description(f"正在下载 / 流量 {flow_size:.2f}MB")
                     if flow_size > self.capacity:
                         executor.shutdown(wait=False, cancel_futures=True)
                         break
 
-        printInfo("===== downloader complete =====")
+        printInfo("===== 下载完成 =====")
         return flow_size
